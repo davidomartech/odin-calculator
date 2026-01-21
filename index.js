cbf674c3c561f2fbf87f81092f2d7793ea46ff9b
David Omar <davidomartech@tutamail.com>
2026-01-21 11:09:33 -0500

    Add operate function to act on operation we pass it

--- index.js
+++ index.js
@@ -14,3 +14,24 @@ function divide(a, b) {
   if (b === 0) return "ERROR: No divide by 0";
   return a / b;
 }
+
+function operate(operator, operandA, operandB) {
+  let result;
+  switch (operator) {
+    case "+":
+      result = add(operandA, operandB);
+      break;
+    case "-":
+      result = subtract(operandA, operandB);
+      break;
+    case "×":
+      result = multiply(operandA, operandB);
+      break;
+    case "÷":
+      result = divide(operandA, operandB);
+      break;
+  }
+  return result;
+}
+
+let operandA, operandB, operator;
