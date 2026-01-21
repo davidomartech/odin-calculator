d649983d66b19117541e38cc58fa90307e73909f
David Omar <davidomartech@tutamail.com>
2026-01-21 12:01:37 -0500

    Add listeners to buttons to make calculator work, only missing delete button function

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ function divide(a, b) {
 }
 
 function operate(operator, operandA, operandB) {
+  operandA = Number(operandA);
+  operandB = Number(operandB);
   let result;
   switch (operator) {
     case "+":
@@ -34,4 +36,61 @@ function operate(operator, operandA, operandB) {
   return result;
 }
 
-let operandA, operandB, operator;
+function clearDisplay() {
+  operandA = "";
+  operandB = "";
+  operator = "";
+  renderDisplay(operandA, operator, operandB);
+}
+
+function renderDisplay(num1 = "", op = "", num2 = "") {
+  if (num1 && num2 && op) {
+    previousOperandDisplay.textContent = `${num1} ${op}`;
+    currentOperandDisplay.textContent = num2;
+  } else if (num1 && !num2 && op) {
+    previousOperandDisplay.textContent = `${num1} ${op}`;
+    currentOperandDisplay.textContent = num2;
+  } else if (num1 && !num2 && !op) {
+    previousOperandDisplay.textContent = "";
+    currentOperandDisplay.textContent = num1;
+  } else {
+    previousOperandDisplay.textContent = "";
+    currentOperandDisplay.textContent = "0";
+  }
+}
+
+let operandA = "",
+  operandB = "",
+  operator = "";
+let calculatorElement = document.querySelector(".calculator");
+let previousOperandDisplay = document.getElementById("previous-operand");
+let currentOperandDisplay = document.getElementById("current-operand");
+let clearBtn = document.getElementById("clear");
+
+clearBtn.addEventListener("click", () => {
+  clearDisplay();
+});
+
+calculatorElement.addEventListener("click", (e) => {
+  if (e.target.classList.contains("number")) {
+    if (!operandB && !operator) {
+      operandA += e.target.textContent;
+      renderDisplay(operandA);
+    } else if (operandA && operator) {
+      operandB += e.target.textContent;
+      renderDisplay(operandA, operator, operandB);
+    }
+  } else if (e.target.classList.contains("operator")) {
+    if (operandA) {
+      operator = e.target.textContent;
+      renderDisplay(operandA, operator);
+    }
+  } else if (e.target.classList.contains("operator-equal")) {
+    if (operandA && operandB && operator) {
+      operandA = operate(operator, operandA, operandB);
+      operandB = "";
+      operator = "";
+      renderDisplay(operandA);
+    }
+  }
+});
